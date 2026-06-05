c3499107c7028273882063e4ae99eab843bffd6c
Sagnick Chatterjee <sagnickchatterjee3@gmail.com>
2026-06-05 11:41:58 +0530

    server.js

--- server.js
+++ server.js
@@ -157,3 +157,6 @@ app.get('/api/requests/profile', authMiddleware, async (req, res) => {
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running smoothly on port ${PORT}`));
+
+
+//Server
